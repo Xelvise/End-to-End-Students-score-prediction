ec2a8b6b51acdac1d0238402ec7d449b0a0a0557
Xelvis <elvisgideonuzuegbu@gmail.com>
2024-01-06 12:38:58 +0100

    Added an API endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from src.pipeline.predict_pipeline import DataChannel, predict
 
 app = Flask(__name__)
@@ -25,6 +25,23 @@ def predict_datapoint():
         df = data.switch_to_df()
         output = predict(df)
         return render_template('home.html', results=output[0])
+
+# Creating an API endpoint url
+@app.route('/api-predict', methods=['POST'])
+def pred():
+    dict = request.get_json()
+    data = DataChannel(
+        gender = dict['gender'],
+        race_ethnicity = dict['ethnicity'],
+        parental_level_of_education = dict['parental_level_of_education'],
+        lunch = dict['lunch'],
+        test_preparation_course = dict['test_preparation_course'],
+        reading_score = dict['reading_score'],
+        writing_score = dict['writing_score']
+    )
+    df = data.switch_to_df()
+    output = predict(df)
+    return jsonify({'maths_score': output[0]}), 200
 
 
 if __name__ == '__main__':
