2d61672a82525cb0a23da519f74e573cef71ef23
Xelvis <elvisgideonuzuegbu@gmail.com>
2024-01-13 18:45:25 +0100

    redirect index page to predict

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 from src.pipeline.predict_pipeline import DataChannel, predict
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect('/predict-data')
 
 @app.route('/predict-data', methods=['GET','POST'])
 def predict_datapoint():
@@ -27,7 +27,7 @@ def predict_datapoint():
         return render_template('home.html', results=output[0])
 
 # Creating an API endpoint url
-@app.route('/api-predict', methods=['POST'])
+@app.route('/api/predict', methods=['POST'])
 def pred():
     dict = request.get_json()
     data = DataChannel(
@@ -45,4 +45,4 @@ def pred():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
